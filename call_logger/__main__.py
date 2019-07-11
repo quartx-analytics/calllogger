2fe2665d0a2a60a8fb9439eed4ab12bc59911eef
willforde <willforde@gmail.com>
2019-07-11 04:41:18 +0100

    initial cleanup backup

--- call_logger/__main__.py
+++ call_logger/__main__.py
@@ -1,32 +1,9 @@
-# Standard library
-import argparse
-import os
-
 # Package imports
-from call_logger import config, setup_env, reporter, arguments
+from call_logger import setup_env, reporter
 from call_logger.monitors import monitor
-
-# Create Parser to parse the required arguments
-parser = argparse.ArgumentParser(description="Call logger")
-for argument in arguments:
-    parser.add_argument(*argument["args"], **argument["kwargs"])
 
 
 def main():
-    # Fetch authentication token from command line
-    args = parser.parse_args()
-
-    # Load the mocked monitor if required
-    if args.simulate:
-        if args.simulate is True:
-            path = args.simulate
-        else:
-            path = os.path.realpath(os.path.expanduser(os.path.expandvars(args.simulate)))
-
-        # Save the mock setting as a system
-        config["settings"]["phone_system"] = "mockcalls"
-        config["mockcalls"] = {"simulate": str(path), "delay": args.delay, "disable_incoming": args.disable_incoming}
-
     # Start the monitor script
     reporters = reporter.spawn_reporters()
     monitor(*reporters)
--- call_logger/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CallLoggerConfig(AppConfig):
-    name = "call_logger"
-    verbose_name = "Phone system call monitoring frontend."

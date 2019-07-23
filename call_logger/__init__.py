989de34cd096674b9f13edfa20dcad0483562dc1
willforde <willforde@gmail.com>
2019-07-23 23:27:27 +0100

    relocate logging

--- call_logger/__init__.py
+++ call_logger/__init__.py
@@ -1,7 +1,40 @@
+# Standard library
+import logging
+import sys
+
 # Package
 from call_logger.config import compile_settings
 
 __all__ = ["settings"]
+
+
+class CusstomStreamHandler(logging.StreamHandler):
+    """
+    A handler class which writes logging records, appropriately formatted, to a stream.
+    Debug & Info records will be logged to sys.stdout, and all other records will be logged to sys.stderr.
+    """
+
+    # noinspection PyBroadException
+    def emit(self, record):
+        """Swap out the stderr stream with stdout if log level is DEBUG or INFO."""
+        if record.levelno <= 20:
+            org_stream = self.stream
+            self.stream = sys.stdout
+            try:
+                super(CusstomStreamHandler, self).emit(record)
+            finally:
+                self.stream = org_stream
+        else:
+            super(CusstomStreamHandler, self).emit(record)
+
+
+# Configure the logging
+cli_handler = CusstomStreamHandler()
+cli_handler.setFormatter(logging.Formatter("%(levelname)-5s: %(message)s"))
+cli_handler.setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.addHandler(cli_handler)
+logger.setLevel(logging.DEBUG)
 
 # The global call logger settings
 settings = compile_settings()
--- call_logger/config.py
+++ call_logger/config.py
@@ -1,7 +1,5 @@
 # Standard lib
-import logging
 import yaml
-import sys
 import os
 
 # Third party
@@ -11,26 +9,6 @@ import appdirs
 CONFIG_DIR = appdirs.site_config_dir("glaonna")
 BASE_CONFIG = os.path.join(os.path.dirname(__file__), "data", "default.yml")
 USER_CONFIG = os.path.join(CONFIG_DIR, "call-logger.yml")
-
-
-class CusstomStreamHandler(logging.StreamHandler):
-    """
-    A handler class which writes logging records, appropriately formatted, to a stream.
-    Debug & Info records will be logged to sys.stdout, and all other records will be logged to sys.stderr.
-    """
-
-    # noinspection PyBroadException
-    def emit(self, record):
-        """Swap out the stderr stream with stdout if log level is DEBUG or INFO."""
-        if record.levelno <= 20:
-            org_stream = self.stream
-            self.stream = sys.stdout
-            try:
-                super(CusstomStreamHandler, self).emit(record)
-            finally:
-                self.stream = org_stream
-        else:
-            super(CusstomStreamHandler, self).emit(record)
 
 
 def update_recursively(base, custom):
@@ -43,14 +21,6 @@ def update_recursively(base, custom):
 
 
 def compile_settings() -> dict:
-    # Configure the logging
-    cli_handler = CusstomStreamHandler()
-    cli_handler.setFormatter(logging.Formatter("%(levelname)-5s: %(message)s"))
-    cli_handler.setLevel(logging.INFO)
-    logger = logging.getLogger("call_logger")
-    logger.addHandler(cli_handler)
-    logger.setLevel(logging.DEBUG)
-
     with open(BASE_CONFIG) as stream:
         settings = yaml.safe_load(stream.read())
 

1aa27ffcc39e41de8da0b592c42c0ff024809eb7
willforde <willforde@gmail.com>
2019-08-10 02:15:59 +0100

    fix docs issue with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,9 @@ extensions = [
     'sphinx.ext.viewcode'
 ]
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

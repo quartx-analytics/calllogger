e4cab3d4d6e5aa2a8fbdecda160b3646abed585a
willforde <willforde@gmail.com>
2021-03-29 02:53:50 +0100

    test import-mode=importlib again

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,7 @@ defaults = dict(
     plugin="MockCalls",
 )
 
+# TODO: Find a better fix for this. This breaks in pytest with --import-mode=importlib
 # Add test defaults to environment
 for key, val in defaults.items():
     if key not in os.environ:

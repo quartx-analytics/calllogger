3f7a38973bcaef752197690393d8f799c78d8f43
willforde <willforde@gmail.com>
2021-05-20 00:28:03 +0100

    partly undo last datastore move to conf

--- src/calllogger/conf.py
+++ src/calllogger/conf.py
@@ -1,20 +1,18 @@
 # Standard lib
 from functools import cached_property
 from pathlib import PosixPath
-from typing import Union
 import logging
 import sys
 import os
 
 # Third Party
 from decouple import config, undefined, UndefinedValueError
-from getmac import get_mac_address
 import appdirs
 
 # Local
-from calllogger import api, utils
+from calllogger.utils import decode_env
 
-__all__ = ["settings", "merge_settings", "get_token", "revoke_token", "get_identifier"]
+__all__ = ["settings", "merge_settings"]
 logger = logging.getLogger(__name__)
 
 
@@ -73,11 +71,11 @@ class Settings:
 
     @cached_property
     def sentry_dsn(self) -> str:
-        return utils.decode_env("SENTRY_DSN")
+        return decode_env("SENTRY_DSN")
 
     @cached_property
     def reg_key(self) -> str:
-        return utils.decode_env("REG_KEY")
+        return decode_env("REG_KEY")
 
     @cached_property
     def datastore(self) -> PosixPath:
@@ -104,57 +102,3 @@ class Settings:
 
 
 settings = Settings()
-token_store = settings.datastore.joinpath("token")
-identifier_store = settings.datastore.joinpath("identifier")
-
-
-def get_identifier() -> Union[str, None]:
-    """Get the unique identifier for this device."""
-    # Stored locally
-    if identifier_store.exists():
-        logger.debug("Loading device identifier from datastore.")
-        return utils.read_datastore(identifier_store)
-
-    # Fetch and save
-    identifier = get_mac_address()
-    if identifier and identifier != "00:00:00:00:00:00":
-        logger.debug("Storing device identifier to datastore.")
-        utils.write_datastore(identifier_store, identifier)
-        return identifier
-    else:
-        return None
-
-
-def get_token() -> str:
-    # Option 1: Environment Variable
-    if "TOKEN" in os.environ:
-        logger.debug("Loading token from environment variable.")
-        return os.environ["TOKEN"]
-
-    # Option 2: Stored locally
-    elif token_store.exists():
-        logger.debug("Loading token from datastore.")
-        return utils.read_datastore(token_store)
-
-    # Option 3: Register with server
-    elif (identifier := get_identifier()) and settings.reg_key:
-        logger.debug("Registering device with server.")
-        token = api.link_device(identifier)
-        utils.write_datastore(token_store, token)
-        logger.debug("Device token received, writing to datastore.")
-        return token
-    else:
-        if identifier is None:
-            logger.debug("Device registration unavailable. Missing required device identifier")
-        if not settings.reg_key:
-            logger.debug("Device registration unavailable. Missing required registration key")
-
-        print("Unable to proceed, missing required TOKEN.")
-        print("Please set the TOKEN environment variable")
-        sys.exit(1)
-
-
-def revoke_token():
-    """Remove token form datastore if exists."""
-    logger.info("Removing stored token if exists")
-    token_store.unlink(missing_ok=True)
---
+++ src/calllogger/datastore.py
@@ -0,0 +1,69 @@
+# Standard Lib
+from typing import Union
+import logging
+import sys
+import os
+
+# Third Party
+from getmac import get_mac_address
+
+# Local
+from calllogger import api, utils
+from calllogger.conf import settings
+
+__all__ = ["get_token", "revoke_token", "get_identifier"]
+logger = logging.getLogger(__name__)
+token_store = settings.datastore.joinpath("token")
+identifier_store = settings.datastore.joinpath("identifier")
+
+
+def get_identifier() -> Union[str, None]:
+    """Get the unique identifier for this device."""
+    # Stored locally
+    if identifier_store.exists():
+        logger.debug("Loading device identifier from datastore.")
+        return utils.read_datastore(identifier_store)
+
+    # Fetch and save
+    identifier = get_mac_address()
+    if identifier and identifier != "00:00:00:00:00:00":
+        logger.debug("Storing device identifier to datastore.")
+        utils.write_datastore(identifier_store, identifier)
+        return identifier
+    else:
+        return None
+
+
+def get_token() -> str:
+    # Option 1: Environment Variable
+    if "TOKEN" in os.environ:
+        logger.debug("Loading token from environment variable.")
+        return os.environ["TOKEN"]
+
+    # Option 2: Stored locally
+    elif token_store.exists():
+        logger.debug("Loading token from datastore.")
+        return utils.read_datastore(token_store)
+
+    # Option 3: Register with server
+    elif (identifier := get_identifier()) and settings.reg_key:
+        logger.debug("Registering device with server.")
+        token = api.link_device(identifier)
+        utils.write_datastore(token_store, token)
+        logger.debug("Device token received, writing to datastore.")
+        return token
+    else:
+        if identifier is None:
+            logger.debug("Device registration unavailable. Missing required device identifier")
+        if not settings.reg_key:
+            logger.debug("Device registration unavailable. Missing required registration key")
+
+        print("Unable to proceed, missing required TOKEN.")
+        print("Please set the TOKEN environment variable")
+        sys.exit(1)
+
+
+def revoke_token():
+    """Remove token form datastore if exists."""
+    logger.info("Removing stored token if exists")
+    token_store.unlink(missing_ok=True)

fbf4cbd3c61418a46e6193e88ecf6badf705d57a
willforde <willforde@gmail.com>
2021-06-04 17:59:54 +0100

    more test covorage

--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -47,3 +47,13 @@ def test_graceful_exception(mocker: MockerFixture, return_data):
     assert spy_running_clear.called
     if isinstance(return_data, Exception):
         spy_terminate.assert_called_with(signal.SIGINT)
+
+
+def test_main_loop(mocker: MockerFixture):
+    mocked_cdrworker = mocker.patch.object(entrypoint.api, "CDRWorker")
+    mocked_info = mocker.patch.object(entrypoint.api, "get_owner_info")
+    mock_plugin = mocker.MagicMock
+
+    entrypoint.main_loop(mock_plugin)
+
+

5c99529843a24e7f56bb345dff970dcc744e499e
willforde <willforde@gmail.com>
2021-06-05 15:50:04 +0100

    send basic info to server

--- src/calllogger/api/info.py
+++ src/calllogger/api/info.py
@@ -1,22 +1,36 @@
 # Standard lib
 from urllib import parse as urlparse
+import socket
 
 # Local
-from calllogger import settings
+from calllogger import settings, __version__
 from calllogger.api import QuartxAPIHandler
 from calllogger.utils import TokenAuth
 
 info_url = urlparse.urljoin(settings.domain, "/api/v1/monitor/cdr/info/")
 
 
+def get_local_ip() -> str:
+    hostname = socket.gethostname()
+    return socket.gethostbyname(hostname)
+
+
 def get_owner_info(token: TokenAuth, identifier) -> dict:
     """Request information about the client."""
     api = QuartxAPIHandler()
-    params = {}
+
+    # We will pass data to server using query params
+    params = dict(
+        version=__version__
+    )
 
     # Add identifier as query parameter
     if identifier:
-        params["device_id"] = str(identifier)
+        params["device_id"] = identifier
+
+    # Add the local IP address
+    if local_ip := get_local_ip():  # pragma: no branch
+        params["local_ip"] = local_ip
 
     resp = api.make_request(
         method="GET",

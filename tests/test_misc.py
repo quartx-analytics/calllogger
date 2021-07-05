374dad9a4b2e41ec44ceef93dfeccba0bc6517e5
willforde <willforde@gmail.com>
2021-07-06 00:39:08 +0100

    check test coverage

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -6,8 +6,7 @@ import pytest
 from pytest_mock import MockerFixture
 
 # Local
-from calllogger import misc
-from calllogger import running
+from calllogger import misc, running, closeers
 
 
 class TestThreadExceptionManager:
@@ -79,3 +78,43 @@ def test_graceful_exception(mocker: MockerFixture, return_data):
     assert spy_running_clear.called
     if isinstance(return_data, Exception):
         spy_terminate.assert_called_with(signal.SIGINT)
+
+
+class Testterminate:
+    def test_terminate(self):
+        """Test that terminate clears the running event flag."""
+        running.set()
+        misc.terminate(signal.SIGINT)
+        assert not running.is_set()
+
+    @pytest.mark.parametrize("event,value", [(signal.SIGINT, 130), (signal.SIGTERM, 143)])
+    def test_terminate_exit_code(self, event, value):
+        """Test that terminate returns the right exit code for the right signal"""
+        running.set()
+        ret = misc.terminate(event)
+        assert not running.is_set()
+        assert ret == value
+
+    @pytest.mark.parametrize("error", [False, True])
+    def test_terminate_call_close(self, error):
+        """Test that terminate clears the running event flag."""
+        closed = False
+
+        def close():
+            nonlocal closed
+            closed = True
+            if error:
+                raise RuntimeError
+
+        # Keep org to reset back later
+        org_closers = closeers[:]
+        closeers.append(close)
+        running.set()
+
+        misc.terminate(signal.SIGINT)
+        assert not running.is_set()
+        assert closed
+
+        # Reset back to org
+        closeers.clear()
+        closeers.extend(org_closers)

286dd16e7443915154b635cd5c3ec657bfc0e3a1
willforde <willforde@gmail.com>
2021-07-06 14:51:02 +0100

    wait for fluent to be running before given up

--- src/calllogger/telemetry/logs.py
+++ src/calllogger/telemetry/logs.py
@@ -1,5 +1,7 @@
 # Standard Lib
+import threading
 import logging
+import socket
 
 # Third Party
 from fluent.handler import FluentRecordFormatter
@@ -9,23 +11,37 @@ from fluent.asynchandler import FluentHandler
 from calllogger import settings, closeers
 
 logger = logging.getLogger("calllogger")
+FLUENT_ADDRESS = "localhost"
+FLUENT_PORT = 24224
 
 
-def setup_remote_logs(client: str):
-    # noinspection PyTypeChecker
-    formatter = FluentRecordFormatter(dict(
-        logger='%(name)s',
-        level='%(levelname)s',
-        thread="%(threadName)s",
-        identifier=settings.identifier,
-        client=client,
-    ))
-    handler = FluentHandler(
-        queue_circular=True,
-        tag="calllogger",
-        host="localhost",
-        port=24224,
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    closeers.append(handler.close)
+def service_available(address: str, port: int):
+    """Check that service at given address is available."""
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result_of_check = a_socket.connect_ex((address, port))
+    return result_of_check == 0
+
+
+def setup_remote_logs(client: str, retry=0):
+    if service_available(FLUENT_ADDRESS, FLUENT_PORT):
+        # noinspection PyTypeChecker
+        formatter = FluentRecordFormatter(dict(
+            logger='%(name)s',
+            level='%(levelname)s',
+            thread="%(threadName)s",
+            identifier=settings.identifier,
+            client=client,
+        ))
+        handler = FluentHandler(
+            queue_circular=True,
+            tag="calllogger",
+            host="localhost",
+            port=24224,
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        closeers.append(handler.close)
+        logger.info("Connection made to local fluent server.")
+    elif retry < 5:
+        logger.info("Local fluent server not available yet. Waiting...")
+        threading.Timer(3, setup_remote_logs, args=[client, retry + 1])
--- tests/telemetry/test_logs.py
+++ tests/telemetry/test_logs.py
@@ -1,12 +1,26 @@
+# Standard Lib
+import socket
+
 # Local
 from calllogger.telemetry.logs import setup_remote_logs
 from calllogger import closeers
 
 
-def test_setup_remote_logs():
-    """Test that fluent logging works with raising an error."""
+def test_setup_remote_logs(mocker):
+    """Test that fluent logging works without raising an error."""
+    mocker.patch.object(socket.socket, "connect_ex", return_value=0)
     current_closers = len(closeers)
     setup_remote_logs("user")
 
-    # There is not much we can test for here
+    # There should be 1 extra close object registered
     assert len(closeers) == current_closers + 1
+
+
+def test_setup_remote_logs_socket_fail(mocker):
+    """Test that fluent logging works without raising an error."""
+    mocker.patch.object(socket.socket, "connect_ex", return_value=1)
+    current_closers = len(closeers)
+    setup_remote_logs("user")
+
+    # There should be no new close object registered
+    assert len(closeers) == current_closers
